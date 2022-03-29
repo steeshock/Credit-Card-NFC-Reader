4278f303c5312608716e4efd4f479f633a844db0
Eugeny Nikolaev <steeshock@gmail.com>
2022-03-29 12:48:13 +0300

    Fix crash on Android SDK >= 31

--- creditCardNfcReader/src/main/java/com/pro100svitlo/creditCardNfcReader/utils/CardNfcUtils.java
+++ creditCardNfcReader/src/main/java/com/pro100svitlo/creditCardNfcReader/utils/CardNfcUtils.java
@@ -12,22 +12,34 @@ import android.nfc.tech.NfcA;
 /**
  * Created by pro100svitlo on 31.03.16.
  */
-public class CardNfcUtils {
+ublic class CardNfcUtils {
 
     private final NfcAdapter mNfcAdapter;
     private final PendingIntent mPendingIntent;
     private final Activity mActivity;
     private static final IntentFilter[] INTENT_FILTER = new IntentFilter[] {
             new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED),
-            new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)};
-    private static final String[][] TECH_LIST = new String[][] { {
-            NfcA.class.getName(), IsoDep.class.getName() } };
+            new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED)
+    };
+    private static final String[][] TECH_LIST = new String[][] {
+            {
+                    NfcA.class.getName(), IsoDep.class.getName()
+            }
+    };
 
     public CardNfcUtils(final Activity pActivity) {
         mActivity = pActivity;
         mNfcAdapter = NfcAdapter.getDefaultAdapter(mActivity);
-        mPendingIntent = PendingIntent.getActivity(mActivity, 0,
-                new Intent(mActivity, mActivity.getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+
+        mPendingIntent = PendingIntent.getActivity(
+                mActivity,
+                0,
+                new Intent(
+                        mActivity,
+                        mActivity.getClass()
+                ).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
+                getFlagsForPendingIntent()
+        );
     }
 
     public void disableDispatch() {
@@ -39,6 +51,22 @@ public class CardNfcUtils {
     public void enableDispatch() {
         if (mNfcAdapter != null) {
             mNfcAdapter.enableForegroundDispatch(mActivity, mPendingIntent, INTENT_FILTER, TECH_LIST);
+        }
+    }
+
+    /**
+     * Fix crash on Android SDK >= 31
+     *
+     * Targeting S+ (version 31 and above) requires that one of
+     * FLAG_IMMUTABLE or FLAG_MUTABLE be specified when creating a PendingIntent
+     *
+     * https://developer.android.com/guide/components/intents-filters#DeclareMutabilityPendingIntent
+     */
+    private int getFlagsForPendingIntent() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            return PendingIntent.FLAG_MUTABLE;
+        } else {
+            return 0;
         }
     }
 }
